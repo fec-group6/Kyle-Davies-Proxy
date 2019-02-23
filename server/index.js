166df2c94e1efcea06d039dc081fdf84694df140
Kyle Davies <kdavies311@gmail.com>
2019-02-23 14:16:08 -0800

    proxy test

--- server/index.js
+++ server/index.js
@@ -6,10 +6,55 @@ const cors = require('cors');
 let app = express();
 
 app.use(express.static(path.join(__dirname, '../public')));
-
+// getting my video list
 app.use(
   '/api/videos', cors(), proxy({
-    target:'http://127.0.0.1:3000',
+    target:'http://videolistservice-env.6wxafjhpca.us-west-1.elasticbeanstalk.com/',
+    changeOrigin: true
+  })
+);
+// getting the video player
+app.use(
+  '/video-player-service/api/get-video', cors(), proxy({
+    target:'http://127.0.0.1:3001',
+    changeOrigin: true
+  })
+);
+
+
+//getting video details and comments
+
+app.use(
+  '/watch/details', cors(), proxy({
+    target:'http://127.0.0.1:3002',
+    changeOrigin: true
+  })
+);
+
+app.use(
+  '/watch/suggestions', cors(), proxy({
+    target:'http://127.0.0.1:3002',
+    changeOrigin: true
+  })
+);
+
+app.use(
+  '/watch/subscribe', cors(), proxy({
+    target:'http://127.0.0.1:3002',
+    changeOrigin: true
+  })
+);
+
+app.use(
+  '/watch/comment', cors(), proxy({
+    target:'http://127.0.0.1:3002',
+    changeOrigin: true
+  })
+);
+
+app.use(
+  '/watch/commentFeeling', cors(), proxy({
+    target:'http://127.0.0.1:3002',
     changeOrigin: true
   })
 );
